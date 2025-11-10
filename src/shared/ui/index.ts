206035d4aff7582dd09f72901c30e6571f9fc9c8
Romano Ycoy <romanoycoy1@gmail.com>
2025-11-10 18:53:50 +0800

    chore: install reusable shadcn UI components

---
+++ src/shared/ui/index.ts
@@ -0,0 +1,13 @@
+// Form components
+export * from './form';
+export * from './input';
+export * from './label';
+export * from './button';
+export * from './checkbox';
+export * from './select';
+
+// Layout components
+export * from './card';
+export * from './table';
+export * from './dialog';
+export * from './alert';
